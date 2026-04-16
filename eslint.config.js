50c4c2d494ed2f8c30bb348cd7e65bcb6fac2d04
yama-kei <keisuke.yamaguchi@gmail.com>
2026-04-16 12:49:10 -0700

    chore: initialize pnpm project with TypeScript, ESLint, vitest

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        console: "readonly",
+        process: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    },
+  },
+  {
+    ignores: ["dist/", "node_modules/"],
+  },
+];
