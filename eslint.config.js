2118ff849dd686822bee04fbd570b3fc5963380c
yama-kei <keisuke.yamaguchi@gmail.com>
2026-04-16 12:50:00 -0700

    test: add CLI integration tests for all commands

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,14 @@
 import js from "@eslint/js";
+import tsParser from "@typescript-eslint/parser";
 
 export default [
   js.configs.recommended,
   {
+    files: ["src/**/*.ts", "tests/**/*.ts"],
     languageOptions: {
       ecmaVersion: 2022,
       sourceType: "module",
+      parser: tsParser,
       globals: {
         console: "readonly",
         process: "readonly",
